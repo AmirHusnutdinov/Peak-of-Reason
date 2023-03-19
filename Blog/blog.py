0aff933b7bec70f606c451dd589516b33a1a0941
AmirHusnutdinov <amir800900@mail.ru>
2023-03-19 20:53:05 +0500

    blog added

--- Blog/blog.py
+++ Blog/blog.py
@@ -1,3 +1,15 @@
+from flask import render_template
+from Links import about_us, blog, reviews, answers, events, authorization, general
+
+
 class Blog:
     def blog(self):
-        return "Собственно blog"
+        return render_template('blog_page.html',
+                               general=general,
+                               about_us=about_us,
+                               blog=blog,
+                               reviews=reviews,
+                               answers=answers,
+                               events=events,
+                               authorization=authorization
+                               )
