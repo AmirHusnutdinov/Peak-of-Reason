0b74a28e3b0479d49168d6a93cdb69bdbc68511c
ilyas13z <ilyas13z@yandex.ru>
2023-03-19 11:58:23 +0500

    add contact_page

--- About_us/about_us.py
+++ About_us/about_us.py
@@ -1,3 +1,15 @@
+from flask import render_template
+from Links import about_us, blog, reviews, answers, events, authorization, general
+
+
 class About:
     def about(self):
-        return "Собственно about"
+        return render_template('contact_page.html',
+                               general=general,
+                               about_us=about_us,
+                               blog=blog,
+                               reviews=reviews,
+                               answers=answers,
+                               events=events,
+                               authorization=authorization
+                               )
--- Start_page/start_page.py
+++ Start_page/start_page.py
@@ -1,11 +1,12 @@
 from flask import render_template
-from Links import about_us, blog, reviews, answers, events, authorization
+from Links import about_us, blog, reviews, answers, events, authorization, general
 
 
 class StartPage:
 
     def main(self):
         return render_template('start_page.html',
+                               general=general,
                                about_us=about_us,
                                blog=blog,
                                reviews=reviews,
