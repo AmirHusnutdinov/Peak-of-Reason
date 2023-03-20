cc58412e34d6866093134518f6c43bf030862aae
AmirHusnutdinov <amir800900@mail.ru>
2023-03-20 19:40:08 +0500

    the blog with answers to questions has been moved from about us to a separate page

--- Answers/answers.py
+++ Answers/answers.py
@@ -1,3 +1,15 @@
+from flask import render_template
+from Links import about_us, blog, reviews, answers, events, authorization, general
+
+
 class Answers:
     def answers(self):
-        return "Собственно answers"
+        return render_template('answers.html',
+                               general=general,
+                               about_us=about_us,
+                               blog=blog,
+                               reviews=reviews,
+                               answers=answers,
+                               events=events,
+                               authorization=authorization
+                               )
