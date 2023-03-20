158e8135bfb70af7208809294f9bdb690e866cac
AmirHusnutdinov <amir800900@mail.ru>
2023-03-20 21:32:49 +0500

    start_make_db

--- Blog/blog.py
+++ Blog/blog.py
@@ -1,10 +1,12 @@
 from flask import render_template
 from Links import about_us, blog, reviews, answers, events, authorization, general
+from data import db_session
 
 
 class Blog:
     @staticmethod
     def blog():
+        db_session.global_init("db/blogs.db")
         return render_template('blog_page.html',
                                general=general,
                                about_us=about_us,
