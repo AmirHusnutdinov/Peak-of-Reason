72a67e6dac22452b8ad628a57943c5981ed79c0f
AmirHusnutdinov <amir800900@mail.ru>
2023-03-23 17:56:24 +0500

    start to make events

--- Events/events.py
+++ Events/events.py
@@ -1,5 +1,16 @@
+from flask import render_template
+from Links import about_us, blog, reviews, answers, events, authorization, general
+
+
 class Events:
     @staticmethod
     def event():
-        return "Собственно events"
-
+        return render_template('event1.html',
+                               general=general,
+                               about_us=about_us,
+                               blog=blog,
+                               reviews=reviews,
+                               answers=answers,
+                               events=events,
+                               authorization=authorization
+                               )
