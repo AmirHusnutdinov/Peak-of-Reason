40a9285921bb75d69937ac7639ba5f0afa092b11
AmirHusnutdinov <amir800900@mail.ru>
2023-03-26 18:23:39 +0500

    admin version

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 from Start_page.start_page import StartPage
 from Reviews.reviews import Reviews
@@ -48,6 +48,15 @@ def open_event4():
 
 @app.route('/blog')
 def open_blog():
+    db_session.global_init("Blog/db/resources.db")
+    db_sess = db_session.create_session()
+    all_posts = db_sess.query(Post)
+    post_info = []
+    for i in all_posts:
+        post_info.append([i.id, i.photo_name,
+                          i.name, i.signature,
+                          i.link, i.created_date])
+    Blog.blogs_info(post_info)
     return Blog.blog()
 
 
@@ -61,9 +70,22 @@ def open_answers():
     return Answers.answers()
 
 
-@app.route('/admin')
+@app.route('/admin',  methods=['POST', 'GET'])
 def open_admin():
-    return Admin.admin()
+    info = Admin.admin(request.method)
+    if request.method == 'GET':
+        return info
+    elif request.method == 'POST':
+        post = Post()
+        post.photo_name = info[1][0]
+        post.name = info[1][1]
+        post.signature = info[1][2]
+        post.link = info[1][3]
+        post.created_date = info[1][4]
+        db_sess2 = db_session.create_session()
+        db_sess2.add(post)
+        db_sess2.commit()
+        return info[0]
 
 
 @app.route('/about_us')
@@ -71,13 +93,4 @@ def open_about_us():
     return About.about()
 
 
-db_session.global_init("Blog/db/resources.db")
-db_sess = db_session.create_session()
-first_post = db_sess.query(Post)
-post_info = []
-for i in first_post:
-    post_info.append([i.id, i.photo_name,
-                      i.name, i.signature,
-                      i.link, i.created_date])
-Blog.blogs_info(post_info)
 app.run(port=8080, host='127.0.0.1')
