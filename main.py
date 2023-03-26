7b9a231ba8b00546c53688d69d1c5d37fd688f18
ilyas <ilyas13z@yandex.ru>
2023-03-26 20:01:52 +0500

    add users.db

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 
+from Authorization.data import db_session1
+from Authorization.data.users import Users
 from Start_page.start_page import StartPage
 from Reviews.reviews import Reviews
 from Events.events import Events
@@ -10,7 +12,6 @@ from Authorization.account import Account
 from Answers.answers import Answers
 from Admin.admin import Admin
 from About_us.about_us import About
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '__secret_key'
@@ -51,9 +52,31 @@ def open_blog():
     return Blog.blog()
 
 
-@app.route('/authorization')
+@app.route('/authorization', methods=['GET', 'POST'])
 def open_authorization():
-    return Account.account()
+    info = Account.account_login(request.method)
+    if request.method == 'GET':
+        return info
+    elif request.method == 'POST':
+        return info[0]
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def open_register():
+    info = Account.account_register(request.method)
+    if request.method == 'GET':
+        return info
+    elif request.method == 'POST':
+        user = Users()
+        user.email = info[1][0]
+        user.password = info[1][1]
+        user.name = info[1][2]
+        user.surname = info[1][3]
+        db_sess3 = db_session1.create_session1()
+        db_sess3.add(user)
+        db_sess3.commit()
+
+        return info[0]
 
 
 @app.route('/answers')
@@ -80,4 +103,15 @@ for i in first_post:
                       i.name, i.signature,
                       i.link, i.created_date])
 Blog.blogs_info(post_info)
+
+db_session1.global_init1("Authorization/db/users.db")
+db_sess1 = db_session1.create_session1()
+second_post = db_sess1.query(Users)
+users_info = []
+for i in second_post:
+    users_info.append([i.id, i.name,
+                      i.surname, i.email,
+                      i.password, i.is_admin,
+                       i.photo])
+Account.users_info(users_info)
 app.run(port=8080, host='127.0.0.1')
