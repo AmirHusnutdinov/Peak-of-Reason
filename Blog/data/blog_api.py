5b1cfd56eb0def12eaf9bbaced066049a29303a3
AmirHusnutdinov <101730335+AmirHusnutdinov@users.noreply.github.com>
2023-04-22 10:40:55 +0500

    Update blog_api.py

--- Blog/data/blog_api.py
+++ Blog/data/blog_api.py
@@ -1,4 +1,4 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 
 from . import db_session_blog
 from .Post import Post
@@ -47,3 +47,78 @@ def get_posts(post_id):
                  for item in posts if item.id == post_id]
         }
     )
+
+
+@blueprint.route('/api/add_post', methods=['POST'])
+def add_post():
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+
+    elif not all(key in request.json for key in
+                 ['id', 'photo_name', 'name', 'signature', 'link', 'created_date', 'post_text']):
+        return jsonify({'error': 'Bad request'})
+
+    db_sess = db_session_blog.create_session()
+    posts = Post(
+        photo_name=request.json['photo_name'],
+        name=request.json['name'],
+        signature=request.json['signature'],
+        link=request.json['link'],
+        created_date=request.json['created_date'],
+        post_text=request.json['post_text']
+    )
+    db_sess.add(posts)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
+
+
+@blueprint.route('/api/post_update/<int:update_id>', methods=['PUT'])
+def update_post(update_id):
+    if type(update_id) != int:
+        return jsonify({'error': 'Not found'})
+
+    db_sess = db_session_blog.create_session()
+    posts = db_sess.query(Post).all()
+    ids = []
+    for user in posts:
+        ids.append(user.id)
+
+    if update_id not in ids:
+        return jsonify({'error': 'Not such id in db'})
+
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+
+    post_to_update = db_sess.query(Post).filter(Post.id == update_id).first()
+    for key in request.json:
+        if key == 'photo_name':
+            post_to_update.photo_name = request.json['photo_name']
+
+        elif key == 'name':
+            post_to_update.name = request.json['name']
+
+        elif key == 'signature':
+            post_to_update.signature = request.json['signature']
+
+        elif key == 'link':
+            post_to_update.link = request.json['link']
+
+        elif key == 'created_date':
+            post_to_update.created_date = request.json['created_date']
+
+        elif key == 'post_text':
+            post_to_update.post_text = request.json['post_text']
+
+    db_sess.commit()
+    return jsonify({'success': 'UPDATE'})
+
+
+@blueprint.route('/api/del_post/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    db_sess = db_session_blog.create_session()
+    posts = db_sess.query(Post).get(post_id)
+    if not posts:
+        return jsonify({'error': 'Not found'})
+    db_sess.delete(posts)
+    db_sess.commit()
+    return jsonify({'success': 'DELETE'})
